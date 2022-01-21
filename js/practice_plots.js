b3e6907763ce77b7a8c90fa4b218e9df4ec57134
Reed Daniel <reeddaniel@Reeds-MacBook-Pro.local>
2022-01-21 13:37:40 -0600

    initial commit of all files

---
+++ js/practice_plots.js
@@ -0,0 +1,99 @@
+// Plotly.newPlot("linePlot", [{x: [5, 10, 15, 20], y: [3, 6, 9, 12]}]);
+
+//var trace = {
+    //x: ["burrito", "pizza", "chicken"],
+    //y: [10, 18, 5],
+    //type: "bar"
+//};
+
+//var layout = {
+    //title: "Luncheon Survey",
+    //xaxis: {title: "Food Option"},
+    //yaxis: {title: "Number of Respondents"}
+//};
+//Plotly.newPlot("plotArea", trace, layout);
+
+
+//BAR CHART SKILLDRILL
+//var bar = [{
+    //x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    //y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    //type: 'bar'
+//}];
+
+//var template = {
+    //title: "Non-Alcoholic Bar",
+    //xaxis: {title: "Drinks"},
+    //yaxis: {title: "Percent of Drink Ordered"}
+//};
+//Plotly.newPlot('plotArea', bar, template);
+
+// PIE CHART
+var trace = {
+    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: 'pie'
+   };
+   var data = [trace];
+   var layout = {
+    title: "'Pie' Chart",
+   };
+   Plotly.newPlot("plotArea", data, layout);
+
+// example of map()
+var numbers = [1,2,3,4,5];
+var doubled = numbers.map(function(num){
+       return num * 2;
+});
+console.log(doubled);
+
+//using map() to pull specific data from array
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+var cityNames = cities.map(function(city){
+    return city.City;
+});
+console.log(cityNames);
+
+
+//using filter() method
+var numbers = [1,2,3,4,5];
+
+var larger = numbers.filter(function(num){
+    return num > 1;
+});
+
+console.log(larger);
+
+//using sort() method
+var familyAge = [3,2,39,37,9];
+var sortedAge = familyAge.sort((anElement,anotherElement) => anElement -
+anotherElement);
+
+//slice() method
+var integers = [0,1,2,3,4,5];
+var slice1 = integers.slice(0,2);
+
